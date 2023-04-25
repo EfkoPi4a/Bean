395ab4e81ecbd9aa02430b4d038c5434fa5c2bb7
Efko <evtim26@abv.bg>
2023-04-25 15:25:59 +0300

    345

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE efkodb;
+USE efkodb;
+CREATE TABLE potrebiteli(
+id INT,
+name VARCHAR(255)
+,pass VARCHAR(255)
+,mail VARCHAR(255)
+);
+INSERT INTO potrebiteli VALUES
+(1,"admin","admin","admin@local.bg");
